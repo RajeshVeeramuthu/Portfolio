e0a3d649091a04e5dc553b1cd638daf5b6d39865
Rajesh Veeramuthu <140933435+RajeshVeeramuthu@users.noreply.github.com>
2024-12-31 22:34:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 $(document).ready(function() {
-    $("#home")
-    .css({ opacity: 0, position: 'relative' })
-    .slideDown(1000)
-    .animate({ opacity: 1 }, { duration: 1500, queue: false })
-    .animate({ top: "-30px" }, 400)  
-    .animate({ top: "0px" }, 400);
-
+    // Smooth scrolling for navigation links
     $('.nav-link').on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, function() {
+            }, 800, function(){
                 window.location.hash = hash;
             });
+            // Close navbar-collapse on link click
             if ($(window).width() < 992) {
                 $('.navbar-collapse').collapse('hide');
             }
         }
     });
-    $('#sentmailbtn').on('click',function(event){
-        alert("This functionality currently unavailable. Try Whatsapp or Instagram");
-        $('#name').val('');
-        $('#email').val('');
-        $('#message').val('');
-    })
 });
