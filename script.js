514e98d904a2dccb5cc8363a9f5e5cd818a39029
Rajesh Veeramuthu <140933435+RajeshVeeramuthu@users.noreply.github.com>
2026-04-19 08:28:33 +0530

    Refactor smooth scrolling and add typing effect
    Replaced jQuery smooth scrolling with vanilla JavaScript and added a typing effect.

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-$(document).ready(function() {
-    // Smooth scrolling for navigation links
-    $('.nav-link').on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-            // Close navbar-collapse on link click
-            if ($(window).width() < 992) {
-                $('.navbar-collapse').collapse('hide');
-            }
-        }
+// Smooth scroll
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+// Simple typing effect
+const text = "Software Developer | Python | .NET | AI";
+let i = 0;
+function typing() {
+    if (i < text.length) {
+        document.querySelector(".typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, 50);
+    }
+}
+document.querySelector(".typing").innerHTML = "";
+typing();
